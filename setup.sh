3b395a7b4f21d746378fc1b54ff0a3d766ab71ec
zhshize <joe7086543@gmail.com>
2021-05-23 18:01:49 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,24 @@
 if [ -x "$(command -v docker)" ]; then
+    echo "Docker is installed, try to start docker..."
     sudo systemctl start docker
+    echo "Docker is started."
 else
+    echo "Docker is not installed, try to install docker..."
     curl -fsSL https://get.docker.com -o get-docker.sh
     sudo sh get-docker.sh
+    sudo systemctl start docker
+    echo "Docker is isntalled and started."
 fi
+
+if test -f "Dockerfile"; then
+    echo "Dockerfile exists"
+else
+    echo "Dockerfile doesn't exist, try to download from github..."
+    git clone git@github.com:zhshize/data-science-hw2.git
+    cd data-science-hw2
+fi
+
+echo "Start to build docker image..."
+docker build -t mlflow-docker-example -f Dockerfile .
+echo "Docker image 'mlflow-docker-example' is built, run container:"
+docker run -p 8888:8888 -p 5050:5050 -e JUPYTER_ENABLE_LAB=yes mlflow-docker-example
